3ef48cee3ebccb3cceb7f93bb32708c7de1a1c3b
Toshikazu Ban <35644224+banto354@users.noreply.github.com>
2023-05-01 00:25:25 +0000

    [Finish] 2-11

---
+++ lesson/lesson11.rb
@@ -0,0 +1,16 @@
+class Car
+    def run(distance)
+        puts "車は#{distance}キロ走ります。"
+    end
+end
+
+class Truck < Car
+    def run(distance)
+        super
+        puts "大きな荷物を乗せて走ります。"
+    end
+    
+end
+
+bus = Truck.new
+bus.run(5)
